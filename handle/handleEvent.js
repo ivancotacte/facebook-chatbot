925bbf92e88fc308179fc272b8543d65e0ebc14e
Ivan Cotacte <cotactearmenion@gmail.com>
2023-09-19 15:34:33 +0800

    Update: Handle

--- handle/handleEvent.js
+++ handle/handleEvent.js
@@ -1,13 +1,10 @@
-const fs = require("fs");
-const ameClient = require("amethyste-api");
-const ameApi = new ameClient(
-  "365745f69238ead2e433c23bb9ccd972293d3c9553a25fc31f647b4ae047e5b201bc5d94584dfe3afbd79d233ec8bbc85d2f1d610bf9749ddb97a0915e630040"
-);
-
 module.exports = async ({ api, event }) => {
   if (event.logMessageType == "log:subscribe") {
+    require("../src/events/joinNoti")({ api, event });
   } else if (event.logMessageType == "log:unsubscribe") {
+    require("../src/events/leftNoti")({ api, event });
   } else if (event.logMessageType == "log:thread-name") {
+    require("../src/events/GroupChatChangeName")({ api, event });
   } else {
   }
 };
